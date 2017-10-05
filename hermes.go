fd577240f171f9e31e4beece5c15e4a82b195df4
Mihai Todor <todormihai@gmail.com>
2017-10-05 14:57:29 +0100

    Cleanup code

--- hermes.go
+++ hermes.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 	nats "github.com/nats-io/go-nats"
+)
+
+const (
+	inactivityTimeout = time.Second * 15
 )
 
 func main() {
@@ -16,59 +20,66 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to connect to NATS: %s", err)
 	}
-	fmt.Println("Connected to NATS server")
+	log.Infof("Connected to NATS server")
 
 	// Open websocket server
 	//connections := sync.Map{}
 	err = http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		conn, _, _, err := ws.UpgradeHTTP(r, w, nil)
 		if err != nil {
-			fmt.Println(err)
+			log.Errorf("Failed to upgrade HTTP connection: %s", err)
 			return
 		}
 
 		go func() {
-			//defer conn.Close()
-
 			// expect to read user unqiue channel name
 			msg, op, err := wsutil.ReadClientData(conn)
 			if err != nil {
-				fmt.Printf("failed read channel.'%s'", err)
+				log.Errorf("failed read channel: %s", err)
 				return
 			}
-
-			if op == ws.OpClose {
+			if op != ws.OpText {
+				log.Errorf("unexpected OP code received: %d", op)
 				conn.Close()
 				return
 			}
 
 			channel := string(msg)
-			fmt.Printf("Channel subscription request: %s", channel)
+			log.Infof("Channel subscription request: %s", channel)
 
 			closeChan := make(chan struct{})
+			timer := time.NewTimer(inactivityTimeout)
 			subscription, err := nc.Subscribe(channel, func(m *nats.Msg) {
-				fmt.Printf("Received message from NATS: %s\n", string(m.Data))
+				log.Infof("Received msg from NATS: %s = %s", channel, string(m.Data))
 				// The manager will fan this out to all the live connections if there are any
-				errNew := wsutil.WriteServerMessage(conn, op, m.Data)
+				// expect to read user unqiue channel name
+				errNew := wsutil.WriteServerMessage(conn, ws.OpText, m.Data)
 				if errNew != nil {
-					fmt.Println("failed Write to ws", errNew)
+					log.Errorf("failed Write to ws", errNew)
 					conn.Close()
 					closeChan <- struct{}{}
+					return
 				}
+
+				timer = time.NewTimer(inactivityTimeout)
 			})
 			if err != nil {
-				fmt.Println("failed NATS subscribe", err)
+				log.Errorf("failed NATS subscribe", err)
+				return
 			}
 
 			go func() {
-				<-closeChan
-				fmt.Printf("Websocket closed, unsubscribing: %s", channel)
-				subscription.Unsubscribe()
+				select {
+				case <-timer.C:
+					log.Infof("Websocket timeout, unsubscribing: %s", channel)
+					subscription.Unsubscribe()
+				case <-closeChan:
+					log.Infof("Websocket closed, unsubscribing: %s", channel)
+					subscription.Unsubscribe()
+				}
 			}()
-
-			fmt.Println("End of WS handler")
 		}()
 	}))
 
-	fmt.Println("Error starting web server: %s", err)
+	log.Errorf("Error starting web server: %s", err)
 }
